a673da98bee2e2e099e59e504a1a06f34fe26932
Ryan Ehlers <ryan.ehlers@oup.com>
2018-02-25 21:28:37 +0000

    Better variable names

--- app.py
+++ app.py
@@ -8,29 +8,29 @@ class App:
         frame.pack()
         frame.option_add("*font", "Times 16")
 
-        self.w = Message(master, width="480", text="Click OK to copy all the .docx files in the chosen folder and strip all the metadata from the copies.")
-        self.w.pack()
+        self.m1 = Message(master, width="480", text="Click OK to copy all the .docx files in the chosen folder and strip all the metadata from the copies.")
+        self.m1.pack()
 
-        self.x = Message(master, anchor="w", width="480", text="The copies go inside a folder called 'stripped' placed inside the chosen folder.")
-        self.x.pack(fill="x")
+        self.m2 = Message(master, anchor="w", width="480", text="The copies go inside a folder called 'stripped' placed inside the chosen folder.")
+        self.m2.pack(fill="x")
 
-        self.x = Message(master, anchor="w", width="480", text="The originals will be unchanged.", justify="left")
-        self.x.pack(fill="x")
+        self.m3 = Message(master, anchor="w", width="480", text="The originals will be unchanged.", justify="left")
+        self.m3.pack(fill="x")
 
-        self.e = Entry(master, width="40")
-        self.e.pack()
-        self.e.delete(0, END)
-        self.e.insert(0, "V:\\")
+        self.e1 = Entry(master, width="40")
+        self.e1.pack()
+        self.e1.delete(0, END)
+        self.e1.insert(0, "V:\\")
 
-        self.hi_there = Button(text="OK", command=self.strip_data)
-        self.hi_there.pack(side=RIGHT)
+        self.ok = Button(text="OK", command=self.strip_data)
+        self.ok.pack(side=RIGHT)
 
-        self.button = Button(text="Cancel", command=frame.quit)
-        self.button.pack(side=RIGHT)
+        self.cancel = Button(text="Cancel", command=frame.quit)
+        self.cancel.pack(side=RIGHT)
 
     def strip_data(self):
-        print(self.e.get())
-        stripdata(self.e.get())
+        print(self.e1.get())
+        stripdata(self.e1.get())
         root.destroy() #close window when finished
 
 root = Tk()
